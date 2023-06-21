078a2882f7e0ee84348573669ebff1c604b9dae3
Alex Hsieh <alexhsieh12@gmail.com>
2023-06-21 13:36:02 -0700

    push formatting

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -4,9 +4,9 @@ import '../../styles/About.css';
 import mePhoto from '../../images/boiler-plate.png'
 export default function About() {
   return (
-    <div className='bodyContainer'>
+    <div className='bodyContainer' >
       <h1 className='aboutHeader'>About Me</h1>
-      <a><img className="aboutMePhoto" src={mePhoto}></img></a>
+      <a><img className="aboutMePhoto" src={mePhoto} alt='Professional Picture of Alex Hsieh in a white blazer'></img></a>
       <p className='aboutContent'>
         My name is Alex, and I am currently located in Chino Hills, California. I am a current student at the University of California, Riverside studying Business Analytics. While studying business in school,
         I took the incentive to pursue a UCLA full stack web development bootcamp. I have been able to develop many front and back end applications throughout this program and am looking for opportunities to continue to improving my abilities.
